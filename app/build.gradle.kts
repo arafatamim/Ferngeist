8aa3dd68287c8a821ea72b1b687a1997dfc0893b
Tamim Arafat <31634638+arafatamim@users.noreply.github.com>
2026-04-13 01:12:41 +0600

    chore(release): Bump version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.tamimarafat.ferngeist"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0-beta01"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
